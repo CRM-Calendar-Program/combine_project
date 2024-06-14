b0ae5a0700f5138c7a711d9513914b6791f9fafc
junheok797 <jun2342@chungbuk.ac.kr>
2024-06-14 23:15:13 +0900

    modify Display::ShowMenu()

--- combine_project/display.h
+++ combine_project/display.h
@@ -14,18 +14,17 @@ private:
 	int center;
 	int box_x = 30;
 	int box_y = 10;
-	DataHandler* DB;
+	DataHandler& DB;
 public:
-	Display(DataHandler& DB) { this->DB = &DB; }
+	Display(DataHandler& DB) : DB(DB) {}
 	void SetCursorPosition(int x, int y);
+	void CleanDisplay() { system("cls"); }
 	void ShowMenu();
 	void ShowClientMenu();
 	void ShowMileageMenu();
 	void ShowHistoryMenu();
 	void DailyMenu();
 	void Register();
-
-	void CleanDisplay() { system("cls"); }
 };
 
 #endif
